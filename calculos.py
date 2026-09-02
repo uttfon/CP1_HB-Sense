b905d66b9254c75f412225bad26e29d42031106a
Luis Henrique <luis.fonseca0812@gmail.com>
2026-09-02 11:00:29 -0300

    Começo do projeto apenas projetando todos os valores iniciais para conseguirmo realizar as contas posteriormente. Dados apresentados: Modulos, leituras ambientais, tripulação, consumo dos modulos e historico de horas de superficies da apollo.

---
+++ calculos.py
@@ -0,0 +1,5 @@
+def calcular_media(valores):
+    return sum(valores) / len(valores)
+
+def calcular_maior(valores):
+    return max(valores)
--- monitoramento.py
+++ monitoramento.py
@@ -1,1 +1,21 @@
 # Entender a situação atual
+
+from calculos import calcular_maior, calcular_media
+from kit_dados import modulos, leituras_ambientais, tripulacao, consumo_modulos, historico_apollo_superficie
+
+print("==== LEITURAS DE DADOS ====")
+
+for modulo in modulos:
+    print(modulo["nome"], "- Capacidade:", modulo["capacidade"], "- Ocupação:", modulo["ocupacao"])
+
+for leitura in leituras_ambientais:
+    print("Ciclo", leitura["ciclo"], "- Porcentagem de O2:", leitura["o2_pct"], "%")
+
+for pessoas in tripulacao:
+    print("ID:", pessoas["id"], "- Turno:", pessoas["turno"])
+
+for consumo in consumo_modulos:
+    print("Módulo:", consumo["modulo"], "- Consumo de água:", consumo["agua_l_dia"], "- Consumo de energia:", consumo["energia_kwh_dia"])
+
+for historico in historico_apollo_superficie:
+    print("Missão:", historico["missao"], "- Horas de superfície:", historico["horas_superficie"])
